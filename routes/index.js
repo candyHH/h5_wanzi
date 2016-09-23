41ef352c50aaf2cc751c90672c353b0ed715caf9
candyhh <17764576286@163.com>
2016-09-23 11:20:11 +0800

    flga

--- routes/index.js
+++ routes/index.js
@@ -33,12 +33,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/index', function(req, res, next) {
-  var isWechat = false;
+  var isWechat = 0;
   var agentID = req.headers['user-agent'].toLowerCase().search(/(micromessenger)/);
   if (agentID > 0) {
-      isWechat = true;
+      isWechat = 1;
   } else {
-      isWechat = false;
+      isWechat = 0;
   }
   var thisUrl = req.url;
   var shareUrl = encodeURIComponent((global.browserURL + thisUrl).split('#')[0]);
